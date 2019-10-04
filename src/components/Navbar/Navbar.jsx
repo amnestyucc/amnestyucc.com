b8904adc486dc35db73885e82492b33e3ffbeb77
IbrahimFadel <ibrahim.m.fadel@gmail.com>
2019-10-03 21:53:59 -0400

    Navbar updated, figuring out burger

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,47 +1,68 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import Burger from "@animated-burgers/burger-slip";
+import "@animated-burgers/burger-slip/dist/styles.css";
+import { Slider } from 'react-burgers'
+// import Burger from '@animated-burgers/{burger-style}';
+// import '@animated-burgers/{burger-style}/dist/styles.css' 
 
-import { active } from "./styles.module.scss";
+import { active, navTitle } from "./styles.module.scss";
+import { throwStatement } from "@babel/types";
+
+const navlinkClassNames = [];
 
 const Navbar = ({ activeLink }) => {
-  const [classNames, setClassNames] = useState({});
-  const [test, setTest] = useState(active);
+	const [homeIsActive, setHomeIsActive] = useState(false);
+	const [articlesIsActive, setArticlesIsActive] = useState(false);
+	const [contactIsActive, setContactIsActive] = useState(false);
+	const [loginIsActive, setLoginIsActive] = useState(false);
 
-  useEffect(() => {
-    //   activeLink === "Home"
-    switch (activeLink) {
-      case "Home":
-        setClassNames({
-          Home: [active]
-        });
-        break;
-      default:
-        setClassNames({
-          Home: [active]
-        });
-    }
-  });
+	useEffect(() => {
+		switch (activeLink) {
+			case "Home":
+				setHomeIsActive(true);
+				break;
+			case "Articles":
+				setArticlesIsActive(true);
+				break;
+			case "Contact":
+				setContactIsActive(true);
+			case "Login":
+				setLoginIsActive(true);
+			default:
+				setHomeIsActive(true);
+		}
+	}, [activeLink]);
 
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/" className={test}>
-              test
-            </Link>
-            {/* {activeLink == "Home" ? (
-              <Link to="/" className={active}>
-                Home
-              </Link>
-            ) : (
-              <Link to="/">Home</Link>
-            )} */}
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
+	function burgerClicked() {
+		
+	}
+
+	return (
+		<div>
+			<nav>
+				<h1 id={navTitle}>Amnesty UCC</h1>
+				<ul>
+					<li>
+						<Link to="/" className={homeIsActive ? active : navlinkClassNames}>
+							Home
+						</Link>
+					</li>
+					<li>
+						<Link to="/" className={articlesIsActive ? active : navlinkClassNames}>Articles</Link>
+					</li>
+					<li>
+						<Link to="/" className={contactIsActive ? active : navlinkClassNames}>Contact</Link>
+					</li>
+					<li>
+						<Link to="/" className={loginIsActive ? active : navlinkClassNames}>Login/Signup</Link>
+					</li>
+				</ul>
+				{/* <Slider onClick={burgerClicked}></Slider> */}
+				<Burger isOpen={false} onClick={burgerClicked} />
+			</nav>
+		</div>
+	);
 };
 
 export default Navbar;
