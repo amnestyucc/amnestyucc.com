776081a3c4e5843dbdccbce14cf576f493ec5750
IbrahimFadel <ibrahim.m.fadel@gmail.com>
2019-10-05 20:36:24 -0400

    Login state updates based on logged in or not

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { auth } from "../../utils/firebase";
 import Navbar from "../../components/Navbar/NavbarContainer";
 import {
 	title,
@@ -8,46 +9,137 @@ import {
 	loginLabel,
 	loginInput,
 	loginButton,
-	signupLink
+	signupLink,
+	loggedInMessageContainer,
+	logoutButton
 } from "./styles.module.scss";
 
-const Login = ({ login }) => {
-	return (
-		<div>
-			<Navbar />
+export default class Login extends React.Component {
+	constructor(props) {
+		super(props);
 
-			<h1 id={title}>Login</h1>
+		this.state = {
+			user: null
+		};
+	}
 
-			<form id={loginForm}>
-				<Link to="/signup" id={signupLink}>
-					Don't have an account? Sign up here!
-				</Link>
+	render() {
+		return (
+			<div>
+				<Navbar />
 
-				<label className={loginLabel}>Email:</label>
-				<input type="email" className={loginInput} id="emailInput"></input>
-				<label className={loginLabel}>Password:</label>
-				<input
-					type="password"
-					className={loginInput}
-					id="passwordInput"
-				></input>
-				<button
-					id={loginButton}
-					onClick={() => {
-						login(
-							document.getElementById("emailInput").value,
-							document.getElementById("passwordInput").value
-						);
-						document.getElementById("emailInput").value = "";
-						document.getElementById("passwordInput").value = "";
-					}}
-					type="button"
-				>
-					Login
-				</button>
-			</form>
-		</div>
-	);
-};
+				{this.props.user ? (
+					<div>
+						<h1 id={title}>Login</h1>
+						<div id={loggedInMessageContainer}>
+							<h1>You are logged in</h1>
+							<p id={logoutButton} onClick={this.props.logout}>
+								Log out
+							</p>
+						</div>
+					</div>
+				) : (
+					<div>
+						<h1 id={title}>Login</h1>
+						<form id={loginForm}>
+							<Link to="/signup" id={signupLink}>
+								Don't have an account? Sign up here!
+							</Link>
 
-export default Login;
+							<label className={loginLabel}>Email:</label>
+							<input
+								type="email"
+								className={loginInput}
+								id="emailInput"
+							></input>
+							<label className={loginLabel}>Password:</label>
+							<input
+								type="password"
+								className={loginInput}
+								id="passwordInput"
+							></input>
+							<button
+								id={loginButton}
+								onClick={() => {
+									this.props.login(
+										document.getElementById("emailInput").value,
+										document.getElementById("passwordInput").value
+									);
+									document.getElementById("emailInput").value = "";
+									document.getElementById("passwordInput").value = "";
+								}}
+								type="button"
+							>
+								Login
+							</button>
+						</form>
+					</div>
+				)}
+			</div>
+		);
+	}
+}
+
+// import React, { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+
+// import { auth } from "../../utils/firebase";
+// import Navbar from "../../components/Navbar/NavbarContainer";
+// import {
+// 	title,
+// 	loginForm,
+// 	loginLabel,
+// 	loginInput,
+// 	loginButton,
+// 	signupLink
+// } from "./styles.module.scss";
+
+// const Login = ({ login }) => {
+
+// 	return (
+// 		<div>
+// 			<Navbar />
+
+// 			{user != null ? (
+// 				<div>
+// 					<h1 id={title}>Login</h1>
+// 					<form id={loginForm}>
+// 						<Link to="/signup" id={signupLink}>
+// 							Don't have an account? Sign up here!
+// 						</Link>
+
+// 						<label className={loginLabel}>Email:</label>
+// 						<input type="email" className={loginInput} id="emailInput"></input>
+// 						<label className={loginLabel}>Password:</label>
+// 						<input
+// 							type="password"
+// 							className={loginInput}
+// 							id="passwordInput"
+// 						></input>
+// 						<button
+// 							id={loginButton}
+// 							onClick={() => {
+// 								login(
+// 									document.getElementById("emailInput").value,
+// 									document.getElementById("passwordInput").value
+// 								);
+// 								document.getElementById("emailInput").value = "";
+// 								document.getElementById("passwordInput").value = "";
+// 							}}
+// 							type="button"
+// 						>
+// 							Login
+// 						</button>
+// 					</form>
+// 				</div>
+// 			) : (
+// 				<div>
+// 					<h1 id={title}>Login</h1>
+// 					<h1>You are logged in</h1>
+// 				</div>
+// 			)}
+// 		</div>
+// 	);
+// };
+
+// export default Login;
