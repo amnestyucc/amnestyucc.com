9b08833217eadb64a27ae4ff8577aac13dfa6f9a
IbrahimFadel <ibrahim.m.fadel@gmail.com>
2020-02-03 20:32:52 -0500

    this is so shitty

--- src/views/Articles/ArticlesContainer.jsx
+++ src/views/Articles/ArticlesContainer.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
 
-import { withRouter } from "react-router-dom";
-import { title } from "./styles.module.scss";
-import { admins } from "../../utils/admins";
-import { auth, db, storage } from "../../utils/firebase";
-import Articles from "./Articles";
-import { asciiToHex, hexToAscii } from "../../utils/helpers";
+import { withRouter } from 'react-router-dom';
+import { title } from './styles.module.scss';
+import { admins } from '../../utils/admins';
+import { auth, db, storage } from '../../utils/firebase';
+import Articles from './Articles';
+import { asciiToHex, hexToAscii } from '../../utils/helpers';
+import { getArticles } from '../../utils/db';
 
 export default class ArticlesContainer extends React.Component {
 	constructor(props) {
@@ -15,7 +16,7 @@ export default class ArticlesContainer extends React.Component {
 			user: null,
 			admin: false,
 			articles: null,
-			response: false
+			response: false,
 		};
 	}
 
@@ -33,10 +34,12 @@ export default class ArticlesContainer extends React.Component {
 
 	checkIfAdmin = async () => {
 		let email;
-		if (!this.state.user) return;
+		if (!this.state.user) {
+			return;
+		}
 		for (let admin of admins) {
-			db.ref("/users/")
-				.once("value")
+			db.ref('/users/')
+				.once('value')
 				.then(snapshot => {
 					snapshot.forEach(user => {
 						if (user.val().uid === this.state.user.uid) {
@@ -47,7 +50,7 @@ export default class ArticlesContainer extends React.Component {
 				.then(() => {
 					if (email === admin) {
 						this.setState({
-							admin: true
+							admin: true,
 						});
 					}
 				});
@@ -55,68 +58,61 @@ export default class ArticlesContainer extends React.Component {
 	};
 
 	postArticle = () => {
-		this.props.history.push("/postArticle");
+		this.props.history.push('/postArticle');
 	};
 
-	getUrl = (title, author) => {
-		const titleHex = asciiToHex(title);
-		const authorHex = asciiToHex(author);
-		return titleHex + authorHex;
-	};
-
-	getArticles = () => {
-		let articles = [];
-		db.ref("/articles/")
-			.once("value")
-			.then(snapshot => {
-				const numChildren = snapshot.numChildren();
-				if (numChildren === 0) {
-					this.setState({
-						response: true,
-						articles: []
-					});
-				}
-				let count = 0;
-				snapshot.forEach(article => {
-					count++;
-					const Article = article.val();
-					storage
-						.ref("/images/" + Article.image)
-						.getDownloadURL()
-						.then(url => {
-							const articleobj = {
-								title: Article.title,
-								imageUrl: url,
-								date: Article.date,
-								author: Article.author,
-								url: this.getUrl(Article.title, Article.author),
-								body: Article.body
-							};
-							articles.push(articleobj);
-						})
-						.then(() => {
-							if (count === numChildren) {
-								this.setState({
-									articles: articles,
-									response: true
-								});
-							}
-						});
-				});
-			});
+	getArticles = async () => {
+		this.setState({
+			response: true,
+		});
+		// const articles = await getArticles();
+		// console.log(articles);
+		// let articles = [];
+		// db.ref('/articles/')
+		// 	.once('value')
+		// 	.then(snapshot => {
+		// 		const numChildren = snapshot.numChildren();
+		// 		if (numChildren === 0) {
+		// 			this.setState({
+		// 				response: true,
+		// 				articles: [],
+		// 			});
+		// 		}
+		// 		let count = 0;
+		// 		snapshot.forEach(article => {
+		// 			count++;
+		// 			const Article = article.val();
+		// 			storage
+		// 				.ref('/images/' + Article.image)
+		// 				.getDownloadURL()
+		// 				.then(url => {
+		// 					const articleobj = {
+		// 						title: Article.title,
+		// 						imageUrl: url,
+		// 						date: Article.date,
+		// 						author: Article.author,
+		// 						url: this.getUrl(Article.title, Article.author),
+		// 						body: Article.body,
+		// 					};
+		// 					articles.push(articleobj);
+		// 				})
+		// 				.then(() => {
+		// 					if (count === numChildren) {
+		// 						this.setState({
+		// 							articles: articles,
+		// 							response: true,
+		// 						});
+		// 					}
+		// 				});
+		// 		});
+		// 	});
 	};
 
 	render() {
 		if (this.state.response) {
-			return (
-				<Articles
-					admin={this.state.admin}
-					postArticle={this.postArticle}
-					articles={this.state.articles}
-				></Articles>
-			);
+			return <Articles></Articles>;
 		} else {
-			return <h1>Loading...</h1>;
+			return <h1 style={{ color: 'white' }}>Loading...</h1>;
 		}
 	}
 }
