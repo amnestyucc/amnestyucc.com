29dfeb1e612c4450d16ff9f055fca50accee5b2d
IbrahimFadel <ibrahim.m.fadel@gmail.com>
2020-02-19 21:12:27 -0500

    Fixing up some stuff for rob

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -19,60 +19,39 @@ class Home extends React.Component {
 				<h1 id={title}>From the Editor</h1>
 				<div id={editorsNote}>
 					<p className={editorsText}>
-						Established in 1961, Amnesty International aims to promote universal
-						human rights through reporting on governments and non-state actors.
-						Though it’s core principle is to focus on prisoners of conscience,
-						including political dissenters, the barbed-wire-wrapped candle of
-						the London-based organization is now a symbol of human rights
-						activists across the globe. Today, Amnesty International mainly
-						deals with women’s, children’s, minorities’, and indigenous rights,
-						ending torture, abolishing the death penalty, rights of refugees,
-						rights of prisoners of conscience, and protection of human dignity.
-						<br />
-						<br />
-						At Upper Canada College, the Amnesty International Club has three
-						objectives:
-						<br />
-						<br />
+						In 1961, two Portuguese students were imprisoned for raising a toast to freedom. Peter Benenson, a vigilant British lawyer, sparked conversation and action around the world about the issue through an article he published in the Observer. In Benenson’s initiative, the organization Amnesty International was born.
+						<br></br>
+						<br></br>
+						Since Amnesty’s beginning, the organization has been driven to protect human rights around the world. By investigating and revealing facts about violations, lobbying governments and large corporations, and mobilizing millions around the world to take a stand for change, Amnesty strives to support individuals in and/or out of power.
+						<br></br>
+						<br></br>
+						Through the collective effort of organizers to civilian supporters, Amnesty has had many successes. In 1963, Amnesty released Ukrainian Archbishop Josyf Slipyi, a prisoner of conscience in Siberia through protest. In 1972, Amnesty launched it’s first campaign against torture, which was later adopted by the United Nations. Finally, Amnesty was awarded the Nobel Peace Prize in 1977.
+						<br></br>
+						<br></br>
+						“Only when the last prisoner of conscience has been freed, when the last torture chamber has been closed, when the United Nations Universal Declaration of Human Rights is a reality for the world’s people, will our work be done.”
+						<br></br>
+						<br></br>
+						-Peter Benenson, Amnesty International founder
+						<br></br>
+						<br></br>
+						As mentioned on our website (amnestyucc.com), the Amnesty Club at UCC aims to...
 					</p>
 					<ol id={editorsList}>
-						<li>To raise awareness and educate about humanitarian issues</li>
-						<li>To promote and encourage action from the community</li>
-						<li>
-							To create a space for meaningful discussion about human rights
-						</li>
+						<li>Raise awareness and educate about humanitarian issues</li>
+						<li>Promote and encourage action from the community</li>
+						<li>Create a space for meaningful discussion about human rights</li>
 					</ol>
 					<p className={editorsText}>
-						We believe that to fulfill these objectives, an online publication
-						would be integral to the process. We will regularly publish articles
-						about humanitarian issues on this website. We hope that by reading
-						them, you can gain a better understanding of humanitarian crises and
-						affairs around the world. Moreover, we encourage you to engage in
-						discussions about our articles. By creating an account and signing
-						in, you can comment, and reply to other comments, on our articles.
-						We also encourage submissions and feedback from the public through
-						the “Contact Us” tab. We hope that your voices and opinions can be
-						reflected in our club’s work as well. Finally, if you would like to
-						be a part of Amnesty International UCC, we meet Mondays and Tuesdays
-						at lunch in Room 233 (Mon) and 235 (Tues).
-						<br />
-						<br />
-						Special thanks to Ms. Bowker, Ms Hui Bon Hoa, the rest of the
-						Amnesty Exec - Shaan, Bassem, Chris, Robbie, and Jeremy, and every
-						club member for their dedicated work as a part of our group!
-						<br />
-						<br /> Also, this website would not be possible without the
-						dedication and hard work or Ibrahim Fadel, who created and maintains
-						this website.
-						<br />
-						<br />
-						Yours Truly,
-						<br />
-						<br />
-						Phillip Kong, Editor-in-chief
-						<br />
-						<br />
-						<br />
+					This year, the club has chosen to focus our publications by region. Although we intend to examine other areas such as South America, the Middle East, and North America, our first publication of 2020 investigates human rights issues in Asia.
+					<br></br>
+					<br></br>
+					If you’re interested in joining the club or even just writing articles for publications like these, feel free to join the club on ManageBac.
+					<br></br>
+					<br></br>
+					Thanks for your time!
+					<br></br>
+					<br></br>
+					- Robbie Evans
 					</p>
 				</div>
 			</div>
